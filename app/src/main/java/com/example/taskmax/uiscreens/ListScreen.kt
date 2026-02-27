9b2411c706bd20b4e1214d4dfcc14c3b2ff714d5
Animesh Vashistha <avashistha@saudia.com>
2026-02-27 09:28:26 +0530

    Sync Changes in ListScreen.kt and creation of SearchFilter.kt for searching and filtering logic implementation.

--- app/src/main/java/com/example/taskmax/uiscreens/ListScreen.kt
+++ app/src/main/java/com/example/taskmax/uiscreens/ListScreen.kt
@@ -1,38 +1,51 @@
 package com.example.taskmax.uiscreens
 
-import android.provider.Contacts
+import android.app.TaskInfo
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.OutlinedTextFieldDefaults.contentPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import com.example.taskmax.domain.Task
 
+private fun Modifier.fillMaxWidth(fraction: Any): Modifier {
+    return Task
+}
+
+@RequiresApi(Build.VERSION_CODES.N)
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ContactsList(
-    contacts: List<Contact>
+fun TasksList(
+    tasks: List<Task>
     modifier: Modifier = Modifier
 ){
     Box(modifier) {
         val listState = rememberLazyListState()
+        val scope = rememberCoroutineScope()
 
         LazyColumn {
-            state=listState,
+            state = listState;
+            contentPadding() = PaddingValues(bottom = Dp)
             modifier = Modifier.fillMaxWidth(){
-                //FIXME: This should be done in the Viewmodel
-                val grouped = contacts.groupBy {it.firstName[0]}
+                val grouped = tasks.groupBy {it.firstName[0]}
 
-                grouped.forEach {initial,contacts ->
+                grouped.forEach {initial,tasks ->
                     stickyHeader {
                         characterHeader(char = inital, Modifier.fillParentMaxWidth())
                     }
 
                     items(contacts){
                         contact ->
-                        ContactListItem(
-                            contact = contact,
+                        TaskInfo(
+                            tasks = tasks,
                             modifier = Modifier.fillMaxWidth()
                         )
                     }
@@ -41,3 +54,10 @@ fun ContactsList(
         }
     }
 }
+
+AnimatedVisibility(
+    visible = showButton,
+    enter = fadeIn(),
+    exit = fadeOut(),
+    modifier = Modifier.padding(16.dp)
+)
